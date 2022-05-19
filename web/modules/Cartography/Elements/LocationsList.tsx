35f55579c3f304bcd66ffbb2ea41a9fe01e55366
dlandouardKlocel <damien.landouard@klocel.com>
2022-05-19 16:52:10 +0200

    Update locations list to display data and modify utils.tsx to take in charge array in the field sort argument

--- web/modules/Cartography/Elements/LocationsList.tsx
+++ web/modules/Cartography/Elements/LocationsList.tsx
@@ -1,44 +1,124 @@
 import { Button, Space } from 'antd';
 import { locationsData } from 'fake-data/locations';
-import { AppTable } from '@components';
+import { AppTable, ContentSpin } from '@components';
 import { DeleteOutlined, EyeTwoTone, EditTwoTone } from '@ant-design/icons';
+import { useCallback, useEffect, useState } from 'react';
+import {
+    DataQueryType,
+    DEFAULT_ITEMS_PER_PAGE,
+    DEFAULT_PAGE_NUMBER,
+    orderByFormater,
+    PaginationType,
+    useLocations
+} from '@helpers';
 
 export const LocationsList = () => {
+    const [locations, setLocations] = useState<DataQueryType>();
+    const [sort, setSort] = useState<any>(null);
+    const [pagination, setPagination] = useState<PaginationType>({
+        total: undefined,
+        current: DEFAULT_PAGE_NUMBER,
+        itemsPerPage: DEFAULT_ITEMS_PER_PAGE
+    });
+
+    // make wrapper function to give child
+    const onChangePagination = useCallback(
+        (currentPage, itemsPerPage) => {
+            // Re fetch data for new current page or items per page
+            setPagination({
+                total: locations?.count,
+                current: currentPage,
+                itemsPerPage: itemsPerPage
+            });
+        },
+        [setPagination, locations]
+    );
+
+    const { isLoading, data, error } = useLocations(
+        undefined,
+        pagination.current,
+        pagination.itemsPerPage,
+        sort
+    );
+
+    useEffect(() => {
+        if (data) {
+            setLocations(data?.locations); // set articles local state with new data
+            setPagination({
+                ...pagination,
+                total: data?.locations?.count // may change total items
+            });
+        }
+    }, [data]);
+
+    const handleTableChange = async (_pagination: any, _filter: any, sorter: any) => {
+        await setSort(orderByFormater(sorter));
+        console.log(sorter);
+    };
+
     const columns = [
         {
             title: 'd:name',
             dataIndex: 'name',
-            key: 'name'
+            key: 'name',
+            sorter: {
+                multiple: 2
+            },
+            showSorterTooltip: false
         },
         {
             title: 'd:bloc',
-            dataIndex: 'bloc',
-            key: 'bloc'
+            dataIndex: ['block', 'name'],
+            key: ['block', 'name'],
+            sorter: {
+                multiple: 1
+            },
+            showSorterTooltip: false
         },
         {
             title: 'd:aisle',
             dataIndex: 'aisle',
-            key: 'aisle'
+            key: 'aisle',
+            sorter: {
+                multiple: 3
+            },
+            showSorterTooltip: false
         },
         {
-            title: 'commom:column',
+            title: 'common:column',
             dataIndex: 'column',
-            key: 'column'
+            key: 'column',
+            sorter: {
+                multiple: 4
+            },
+            showSorterTooltip: false
         },
         {
             title: 'd:level',
             dataIndex: 'level',
-            key: 'level'
+            key: 'level',
+            sorter: {
+                multiple: 5
+            },
+            showSorterTooltip: false
         },
         {
             title: 'd:position',
             dataIndex: 'position',
-            key: 'position'
+            key: 'position',
+            sorter: {
+                multiple: 6
+            },
+            showSorterTooltip: false
         },
         {
             title: 'd:replenish',
             dataIndex: 'replenish',
-            key: 'replenish'
+            key: 'replenish',
+            sorter: {
+                multiple: 7
+            },
+            showSorterTooltip: false
         },
         {
             title: 'actions:actions',
@@ -57,5 +137,21 @@ export const LocationsList = () => {
         }
     ];
 
-    return <AppTable type="locations" columns={columns} data={locationsData} />;
+    return (
+        <>
+            {locations ? (
+                <AppTable
+                    type="locations"
+                    columns={columns}
+                    data={locations!.results}
+                    pagination={pagination}
+                    isLoading={isLoading}
+                    setPagination={onChangePagination}
+                    onChange={handleTableChange}
+                />
+            ) : (
+                <ContentSpin />
+            )}
+        </>
+    );
 };
